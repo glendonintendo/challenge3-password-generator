18a2f1a98a23094898829fc3118ebdd4df99da3e
glendo <gleneggers64@gmail.com>
2021-02-10 09:52:34 -0600

    add pw creator method with validation of requested character types to characters object; fix recursion to return functions

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,31 @@
 // Assignment code here
-var characters = {
+let characters = {
   lower: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
   upper: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
   number: ["0","1","2","3","4","5","6","7","8","9"],
-  special: [" ","!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"]
-}
+  special: [" ","!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"],
+  compilePassword: function(num, arr) {
+    let pwProto = "";
+
+    let charTypeCheck = [];
+    for (let i = num; i > 0; i--) {
+      let charTypeIndex = Math.floor(Math.random() * arr.length);
+      let charType = arr[charTypeIndex];
+      let newChar = this[charType][Math.floor(Math.random() * this[charType].length)];
+      pwProto += newChar;
+      charTypeCheck.push(arr[charTypeIndex]);
+    }
+    console.log(charTypeCheck);
+    console.log(pwProto);
+    
+    if (arr.every(type => charTypeCheck.includes(type))) {
+      console.log(pwProto);
+      return(pwProto);
+    } else {
+      return this.compilePassword(num, arr);
+    }
+  }
+};
 
 var getNumChar = function() {
   var numChar = parseInt(window.prompt("How many characters long would you like your password to be? Please choose a whole number between 8 and 128 (inclusive). Then click 'OK'."));
@@ -12,7 +33,7 @@ var getNumChar = function() {
     return numChar;
   } else {
     window.alert("Please make sure to enter a whole number between 8 and 128 (inclusive). Then click 'OK'.");
-    getNumChar();
+    return getNumChar();
   }
 };
 
@@ -23,42 +44,29 @@ var getCharBools = function() {
     window.confirm("Numbers?"),
     window.confirm("Special characters?")
   ];
-  console.log(charBools.includes(true));
+
   if (charBools.includes(true)) {
     return charBools;
   } else {
     window.alert("You must choose one 'OK' for one of the following options in order to generate a password. Let's try again.")
-    getCharBools();
+    return getCharBools();
   }
 };
 
 var generatePassword = function() {
-  var pw = "";
   var numCharActual = getNumChar();
   var charBoolsActual = getCharBools();
   
   window.alert("Okay. Generating your password now...");
 
   var selectedCharTypes = [];
-  if (charBoolsActual[0]) {
-    selectedCharTypes.push("lower");
+  for (var i = 0; i < charBoolsActual.length; i++) {
+    if (charBoolsActual[i]) {
+      selectedCharTypes.push(Object.keys(characters)[i]);
+    }
   }
-  if (charBoolsActual[1]) {
-    selectedCharTypes.push("upper");
-  }
-  if (charBoolsActual[2]) {
-    selectedCharTypes.push("number");
-  }
-  if (charBoolsActual[3]) {
-    selectedCharTypes.push("special");
-  }
-  console.log(selectedCharTypes);
 
-  for (var i = numCharActual; i > 0; i--) {
-    var charType = selectedCharTypes[Math.floor(Math.random() * selectedCharTypes.length)];
-    var newChar = characters[charType][Math.floor(Math.random() * characters[charType].length)];
-    pw += newChar;
-  }
+  let pw = characters.compilePassword(numCharActual, selectedCharTypes);
 
   return pw;
 };
