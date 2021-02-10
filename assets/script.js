a20bb48ba12d91636c92e2e53c1302f3a57cedf4
glendo <gleneggers64@gmail.com>
2021-02-10 16:49:50 -0600

    remove test console logs; add cancel generate password if cancel is clicked on first numChar prompt

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 // Assignment code here
 let characters = {
-  lower: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
-  upper: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
+  lowercase: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+  uppercase: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
   number: ["0","1","2","3","4","5","6","7","8","9"],
   special: [" ","!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"],
   compilePassword: function(num, arr) {
@@ -15,11 +15,8 @@ let characters = {
       pwProto += newChar;
       charTypeCheck.push(arr[charTypeIndex]);
     }
-    console.log(charTypeCheck);
-    console.log(pwProto);
-    
+
     if (arr.every(type => charTypeCheck.includes(type))) {
-      console.log(pwProto);
       return(pwProto);
     } else {
       return this.compilePassword(num, arr);
@@ -28,9 +25,14 @@ let characters = {
 };
 
 var getNumChar = function() {
-  var numChar = parseInt(window.prompt("How many characters long would you like your password to be? Please choose a whole number between 8 and 128 (inclusive). Then click 'OK'."));
-  if (typeof numChar === "number" && numChar >= 8 && numChar <= 128) {
+  var numChar = window.prompt("How many characters long would you like your password to be? Please choose a whole number between 8 and 128 (inclusive). Then click 'OK'.");
+  if (!numChar) {
     return numChar;
+  }
+
+  let numCharInt = parseInt(numChar);
+  if (typeof numCharInt === "number" && numChar >= 8 && numChar <= 128) {
+    return numCharInt;
   } else {
     window.alert("Please make sure to enter a whole number between 8 and 128 (inclusive). Then click 'OK'.");
     return getNumChar();
@@ -55,6 +57,9 @@ var getCharBools = function() {
 
 var generatePassword = function() {
   var numCharActual = getNumChar();
+  if (!numCharActual) {
+    return;
+  }
   var charBoolsActual = getCharBools();
   
   window.alert("Okay. Generating your password now...");
@@ -79,7 +84,9 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  if (password) {
+    passwordText.value = password;
+  }
 
 };
 
