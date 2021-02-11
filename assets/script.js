5f5bd07e38e908194989bf91989c848670260632
glendo <gleneggers64@gmail.com>
2021-02-11 15:43:12 -0600

    make pw generation truly random; fix validation of characters present in pw; fix comments; replace var keyword with let/const

--- assets/script.js
+++ assets/script.js
@@ -7,8 +7,8 @@
 
 /* 
   character object holds arrays of acceptable characters and two methods:
-    1. getCharTypes prompts user for each char type in keys, converts those booleans into the key names, and returns an array of those key names
-    2. compilePassword takes the number of requested characters and an array of character types and randomly generates a password character by character
+    1. getCharTypes prompts user for each char type in keys of character objects, converts those booleans into the key names, and returns an array of those key names
+    2. compilePassword takes the number of requested characters and an array of character types and randomly generates a password character by character and then validates that each character is present before returning password
 */
 let characters = {
   lowercase: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
@@ -17,56 +17,70 @@ let characters = {
   special: [" ","!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","\\","]","^","_","`","{","|","}","~"],
 
   getCharTypes: function() {
+    // function to title case a single-word string
     const toTitleCase = function(string) {
       return string.charAt(0).toUpperCase() + string.slice(1);
-    }
+    };
     
-    let charArrayKeys = []
-    for (i = 0; i < Object.keys(this).length; i++) {
+    // generates an array of available character types from character object
+    let charArrayKeys = [];
+    for (let i = 0; i < Object.keys(this).length; i++) {
       if (Array.isArray(this[Object.keys(this)[i]])) {
         charArrayKeys.push(Object.keys(this)[i]);
       }
     }
 
+    // prompts user to schoose whether they want to include each character type in password
     let charBools = [];
     for (let i = 0; i < charArrayKeys.length; i++) {
       let charPrompt;
       if (i === 0) {
-        charPrompt = `Would you like to use ${charArrayKeys[i]} characters in your password?\nClick 'OK' for YES or 'Cancel' for NO.`
+        charPrompt = `Would you like to use ${charArrayKeys[i]} characters in your password?\nClick 'OK' for YES or 'Cancel' for NO.`;
       } else {
-        charPrompt = `${toTitleCase(charArrayKeys[i])} characters?`
+        charPrompt = `${toTitleCase(charArrayKeys[i])} characters?`;
       }
-      charBools.push(window.confirm(charPrompt))
+      charBools.push(window.confirm(charPrompt));
     }
   
+    // validates that at least one character type was chosen
     if (!charBools.includes(true)) {
-      window.alert("You must choose one 'OK' for one of the following options in order to generate a password. Let's try again.")
+      window.alert("You must choose one 'OK' for one of the following options in order to generate a password. Let's try again.");
       return this.getCharTypes();
     }
     
-    let selectedCharTypes = [];
+    // creates and returns an array of char types based on user inputs
+    let charTypes = [];
     for (let i = 0; i < charBools.length; i++) {
       if (charBools[i]) {
-        selectedCharTypes.push(Object.keys(this)[i]);
+        charTypes.push(Object.keys(this)[i]);
       }
     }
-    return selectedCharTypes;
+    return charTypes;
   },
 
   compilePassword: function(num, arr) {
-    let pwProto = "";
-
-    let charTypeCheck = [];
-    for (let i = num; i > 0; i--) {
-      let charTypeIndex = Math.floor(Math.random() * arr.length);
-      let charType = arr[charTypeIndex];
-      let newChar = this[charType][Math.floor(Math.random() * this[charType].length)];
-      pwProto += newChar;
-      charTypeCheck.push(arr[charTypeIndex]);
+    // creates an array from characters properties arrays that user selected
+    let charArray = [];
+    for (let i = 0; i < arr.length; i++) {
+      charArray = charArray.concat(this[arr[i]]);
     }
 
-    if (arr.every(type => charTypeCheck.includes(type))) {
-      return(pwProto);
+    // randomly generates a password from the concatentated character array
+    let password = "";
+    for (let i = num; i > 0; i--) {
+      const newChar = charArray[Math.floor(Math.random() * charArray.length)];
+      password += newChar;
+    }
+
+    // validates that at least one character of each type is present in password, recurs if no, returns password if yes
+    let validateCharTypesArray = [];
+    let splitPassword = password.split("");
+    for (let i = 0; i < arr.length; i++) {
+      validateCharTypesArray.push(this[arr[i]].some(element => splitPassword.includes(element)));
+    }
+
+    if (validateCharTypesArray.every(i => i === true)) {
+      return password;
     } else {
       return this.compilePassword(num, arr);
     }
@@ -78,13 +92,13 @@ let characters = {
   getNumChar asks the user for a number between 8-128, parses it to an integer, and returns that integer
   if 'Cancel' is chosen in the getNumChar prompt, the program will stop generating a password
 */
-var getNumChar = function() {
-  var numChar = window.prompt("How many characters long would you like your password to be? Please choose a whole number between 8 and 128 (inclusive). Then click 'OK'.");
+const getNumChar = function() {
+  const numChar = window.prompt("How many characters long would you like your password to be? Please choose a whole number between 8 and 128 (inclusive). Then click 'OK'.");
   if (!numChar) {
     return numChar;
   }
 
-  let numCharInt = parseInt(numChar);
+  const numCharInt = parseInt(numChar);
   if (typeof numCharInt === "number" && numChar >= 8 && numChar <= 128) {
     return numCharInt;
   } else {
@@ -96,45 +110,45 @@ var getNumChar = function() {
 /*
   takes in previous user inputs about password and creates a string of those inputs
 */
-let getFinalValidate = function (num, arr) {
-  let charTypeString = "";
+const getFinalValidate = function (num, arr) {
+  let charTypesString = "";
   for (i = 0; i < arr.length; i++) {
     if (i === 0) {
-      charTypeString += arr[i];
+      charTypesString += arr[i];
     } else if (i !== arr.length - 1) {
-      charTypeString += ", " + arr[i];
+      charTypesString += ", " + arr[i];
     } else if (arr.length !== 2) {
-      charTypeString += ", and " + arr[i];
+      charTypesString += ", and " + arr[i];
     } else {
-      charTypeString += " and " + arr[i];
+      charTypesString += " and " + arr[i];
     }
   }
-  let validateString = `So, you would like a password that is ${num} characters long and contains ${charTypeString} characters?`;
+  const validateString = `So, you would like a password that is ${num} characters long and contains ${charTypesString} characters?`;
   return validateString;
 };
 
 /* 
-  creates a password using previously defined object properties, object methods, and function expressions
+  creates a password using previously defined object methods and function expressions
 */
-var generatePassword = function() {
+const generatePassword = function() {
   // calls getNumChar to get length of generated password; if numChar is falsy, passes undefined
-  var numChar = getNumChar();
+  const numChar = getNumChar();
   if (!numChar) {
     return;
   }
   
   // calls getCharBools to get a list of booleans in the same position order as character object keys
-  var charTypes = characters.getCharTypes();
+  const charTypes = characters.getCharTypes();
 
   // validates choices a final time
-  let finalValidate = window.confirm(getFinalValidate(numChar, charTypes));
+  const finalValidate = window.confirm(getFinalValidate(numChar, charTypes));
   if (!finalValidate) {
     return generatePassword();
   }
 
   // calls compilePassword from the characters object to generate a password using data from user entries
   window.alert("Okay. Generating your password now...");
-  let password = characters.compilePassword(numChar, charTypes);
+  const password = characters.compilePassword(numChar, charTypes);
 
   return password;
 };
@@ -143,7 +157,7 @@ var generatePassword = function() {
   Get references to the #generate element 
   Add event listener to generate button
 */
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 
 /* 
@@ -151,8 +165,8 @@ generateBtn.addEventListener("click", writePassword);
   if 'Cancel' is clicked in numChar prompt, textarea is not changed
 */
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
 
   if (password) {
     passwordText.value = password;
